0634376e61a2801bb6529245e0f974c2b859c5d4
David <davidma@college.harvard.edu>
2019-06-25 10:08:23 -0400

    fixed bug with unused import statements

--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
@@ -43,15 +43,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.ble.BLEAdvertisingActivity;
-import com.example.android.ble.BLECentralChatEvents;
 import com.example.android.ble.BLECentralHelper;
+import com.example.android.ble.BLECentralChatEvents;
 import com.example.android.ble.BLEDiscoveringActivity;
 import com.example.android.ble.BLEMode;
-//import com.example.android.ble.BLEAdvertisingActivity;
-//import com.example.android.ble.BLECentralHelper;
-//import com.example.android.ble.BLECentralChatEvents;
-//import com.example.android.ble.BLEDiscoveringActivity;
-//import com.example.android.ble.BLEPeripheralChatEvents;
 import com.example.android.ble.BLEPeripheralChatEvents;
 import com.example.android.ble.BLEPeripheralHelper;
 import com.example.android.common.logger.Log;
@@ -322,9 +317,9 @@ public class BluetoothChatFragment extends Fragment {
         public void handleMessage(Message msg) {
             FragmentActivity activity = getActivity();
             switch (msg.what) {
-//                case Constants.MESSAGE_STATE_CHANGE:
-//                    setState(msg.arg1);
-//                    break;
+                case Constants.MESSAGE_STATE_CHANGE:
+                    setState(msg.arg1);
+                    break;
                 case Constants.MESSAGE_WRITE:
                     byte[] writeBuf = (byte[]) msg.obj;
                     // construct a string from the buffer
@@ -406,6 +401,7 @@ public class BluetoothChatFragment extends Fragment {
                 startScanning();
                 return true;
             }
+
         }
         return false;
     }
@@ -503,7 +499,7 @@ public class BluetoothChatFragment extends Fragment {
                     @Override
                     public void run() {
 //                        BLECentralHelper.getInstance().changeMtu(256);
-                        sendMessageViaBLE("/name test");
+                        sendMessageViaBLE("/name testName");
                         sendMessageViaBLE("testing");
                         showConnectedName(mConnectedDeviceName);
                         showStatus(BluetoothChatService.STATE_CONNECTED);
