2e6dfafb0130487ca7c63d1587160f77d7ab3f96
David <davidma@college.harvard.edu>
2019-06-25 16:47:42 -0400

    got central to send automated messages to peripheral after automatic reconnection

--- Application/src/main/java/com/example/android/ble/RestartedActivity.java
+++ Application/src/main/java/com/example/android/ble/RestartedActivity.java
@@ -5,11 +5,25 @@ import android.content.Context;
 
 import com.example.android.common.logger.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class RestartedActivity {
     public static final String TAG = "RestartedActivity";
 
     protected void logTest(){
         Log.i(TAG, "accessed restarted activity!");
+    }
+
+    class SayHello extends TimerTask {
+        public void run() {
+            Log.d("RESTARTDEBUGGER", "Reached TimerTask!");
+            if(BLECentralHelper.getInstance() == null){
+                Log.d("RESTARTDEBUGGER", "Instance is null!");
+            } else {
+                BLECentralHelper.getInstance().send("Hello!");
+            }
+        }
     }
 
     protected void reconnect(){
@@ -20,6 +34,9 @@ public class RestartedActivity {
             BLECentralChatEvents ev = BLECentralHelper.getInstance().getCCE();
 
             BLECentralHelper.getInstance().connect(con, dev, ev);
+
+            Timer timer = new Timer();
+            timer.schedule(new SayHello(), 10000, 2000);
         } else {
             Log.i(TAG, "Uhh.. ConnectedGATT is null");
         }
